c875a39af0825f223d0411ade16adee2e7dd4d1a
quang.tothanh <quang.tothanh@vti.com.vn>
2023-07-26 14:53:30 +0700

    update

--- Family.py
+++ Family.py
@@ -5,9 +5,13 @@ class Parent:
         self.age = age
         self.weight = weight
         self.height = height
+        self.member = []
 
     def speak(self):
         print('Welcome')
+
+    def show_info(self):
+        print(f'Name: {self.name} ,Age: {self.age} ,Weight: {self.weight} ,Height: {self.height}')
 
 
 class Child(Parent):
@@ -16,5 +20,23 @@ class Child(Parent):
         print('Thank you')
 
 
-Child = Child('Tan', 14, 45, '130cm')
-Child.speak()
+class GrandChild(Child):
+    def __init__(self, name, age, weight, height, ID):
+        super().__init__(name, age, weight, height)
+        self.ID = ID
+
+    def show_info(self):
+        print(f'ID: {self.ID} ,Name: {self.name} ,Age: {self.age} ,Weight: {self.weight} ,Height: {self.height}')
+
+
+Parent1 = Parent('Chien', 57, 75, '180cm')
+Parent2 = Parent('Huong', 53, 49, '160cm')
+Child1 = Child('Tan', 14, 45, '130cm')
+Child2 = Child('Hoang', 25, 65, '175cm')
+
+Parent1.speak()
+Child1.speak()
+
+GrandChild1 = GrandChild('Hoa', 18, 42, '170cm', 666999)
+Parent1.show_info()
+GrandChild1.show_info()
